c2686893b1c797bf6b28b361fa7a566475581c2c
Abdi Cheda's MAC <mac@Abdi-Chedas-MacBook-Pro.local>
2023-11-20 18:00:31 +0300

    commit

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -4,13 +4,15 @@ function BotCollection({ bots, addToArmy }) {
   return (
     <div className="BotCollection">
       <h2>All Bots</h2>
-      {bots.map((bot) => (
-        <div key={bot.id}>
-          <h3>{bot.name}</h3>
-          <img src={bot.avatar_url} alt={bot.name} />
-          <button onClick={() => addToArmy(bot)}>Add to Army</button>
-        </div>
-      ))}
+      <div className="botCardContainer">
+        {bots.map((bot) => (
+          <div className="BotCard" key={bot.id}>
+            <h3>{bot.name}</h3>
+            <img src={bot.avatar_url} alt={bot.name} />
+            <button onClick={() => addToArmy(bot)}>Add to Army</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
