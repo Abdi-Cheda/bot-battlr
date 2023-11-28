c008bfc0456d3373e2df0b67aeb5db7f0a5f26ae
Abdi-Cheda <abdicheda75@gmail.com>
2023-11-28 05:43:57 +0300

    commit

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const port = 3000;
 
 app.use(cors());
 
-// Example data (replace this with your actual data source)
 const bots = [
   { id: 1, name: 'Bot 1' },
   { id: 2, name: 'Bot 2' },
-  // Add more bot objects as needed
 ];
 
 app.get('/bots', (req, res) => {
